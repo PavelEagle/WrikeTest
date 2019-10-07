6e6643eedfbd0bfb5c8d74de8e45d6f6d2987177
PavelEagle <47183588+PavelEagle@users.noreply.github.com>
2019-10-07 23:37:50 +0300

    Создание дерева, частичная реализация анимации
    Сделаны следующие этапы:
    1) Посылается Get запрос для получения JSON - файла
    2) На основе полученного массива элементов строится DOM с корнем дерева Id=-1
    3) Частично реализована анимация скрытия/раскрытия

--- src.js
+++ src.js
@@ -1,15 +1,3 @@
-
-
-var toggler = document.getElementsByClassName("caret");
-var i;
-
-for (i = 0; i < toggler.length; i++) {
-  toggler[i].addEventListener("click", function() {
-    this.parentElement.querySelector(".nested").classList.toggle("active");
-    this.classList.toggle("caret-down");
-  });
-}
-
 const http = new XMLHttpRequest();
 
 http.open("GET", "https://raw.githubusercontent.com/wrike/frontend-test/master/data.json",false);
@@ -19,35 +7,49 @@ const tree = JSON.parse(http.responseText);
 
 console.log(tree);
 
-let mainTree = Object.values(tree).filter(x=>x.parentId==null);
-
-let sortTree = Object.values(tree).filter(x=>x.parentId!=null).sort((a,b)=>a.parentId-b.parentId);
-
-let total = {...mainTree, ...sortTree};
-
-console.log(total);
-
-tree.forEach(function (key) { 
-    // console.log(key.parentId);
-    if (key.parentId==-1) {
-        let test = document.getElementById('test');
-        let ulObj = document.createElement('ul');
-        let liObj = document.createElement('li');
-        liObj.innerText= key.title;
-        test.appendChild(ulObj); 
-        ulObj.appendChild(liObj); 
-    }
-    else {
-        let test = document.getElementById('test');
-        let liObj = document.createElement('li');
-        let ulObj = document.createElement('ul');
-        liObj.innerText= key.title;
-        test.appendChild(ulObj); 
-        ulObj.appendChild(liObj);
-    }
+let sortTree = Object.values(tree).sort(function (a,b) {
+  if (a.parentId==null) {
+    return -1;
+  }
+  return a.parentId-b.parentId;
 });
 
+console.log(sortTree);
 
-// http.onload = () => console.log(response1);
-
-
+sortTree.forEach(function (key) { 
+    // console.log(key.parentId);
+    if (key.parentId==null) {
+      let test = document.getElementById('test');
+      let ulObj = document.createElement('ul');
+      let liObj = document.createElement('li');
+      let span = document.createElement('span');
+      test.appendChild(ulObj); 
+      ulObj.appendChild(liObj); 
+      liObj.appendChild(span);
+      span.innerText = key.title;
+      liObj.id = 'first-level';
+      span.className = 'caret';
+    }
+    if (key.parentId==-1) {
+      let firstLevel= document.getElementById('first-level');
+      let ulObj = document.createElement('ul');
+      let liObj = document.createElement('li');
+      let span = document.createElement('span');
+      firstLevel.appendChild(ulObj); 
+      ulObj.appendChild(liObj); 
+      liObj.appendChild(span);
+      span.innerText = key.title;
+      liObj.id = 'second-level';
+      span.className = 'caret';
+      ulObj.className = 'nested';
+    }
+    if (key.parentId>=1) {
+        let secondLevel = document.getElementById('second-level');
+        let liObj = document.createElement('li');
+        let ulObj = document.createElement('ul');
+        liObj.innerText = key.title;
+        secondLevel.appendChild(ulObj); 
+        ulObj.appendChild(liObj);
+        ulObj.className = 'nested';
+    }
+});
