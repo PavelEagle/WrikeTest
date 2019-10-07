3eaa74ceffc8ac161a9d409af6e09c188e3217a8
Pavel <pavel.eagle1@gmail.com>
2019-10-07 23:41:29 +0300

    Добавление анимации
    Частично реализована анимация скрытия/раскрытия

---
+++ caret.js
@@ -0,0 +1,10 @@
+let toggler = document.getElementsByClassName("caret");
+
+console.log(toggler.length);
+
+for (let i = 0; i < toggler.length; i++) {
+  toggler[i].addEventListener("click", function() {
+    this.parentElement.querySelector(".nested").classList.toggle("active");
+    this.classList.toggle("caret-down");
+  });
+}
