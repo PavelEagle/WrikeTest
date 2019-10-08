d66f9f579c23393a26f5fdfdbdd81a66f91461be
Pavel <pavel.eagle1@gmail.com>
2019-10-09 00:05:59 +0300

    Анимация, комментарии
    1) Доработана анимация (теперь открываюся все элементы, а не только 1);
    2) Добавлены Id для создания дерева (в прошлой версии не все работало корректно)
    3) Добавлен тестовый файл JSON
    4) Добавлены некоторые комментарии

--- caret.js
+++ caret.js
@@ -1,10 +1,9 @@
-let toggler = document.getElementsByClassName("caret");
+let toggler = document.getElementsByClassName("caret"); //Поиск всех элементов со стилем caret
 
-console.log(toggler.length);
-
-for (let i = 0; i < toggler.length; i++) {
-  toggler[i].addEventListener("click", function() {
-    this.parentElement.querySelector(".nested").classList.toggle("active");
-    this.classList.toggle("caret-down");
+for (let i = 0; i < toggler.length; i++) { 
+  toggler[i].addEventListener("click", function() {                       // Для каждого элемента со стилем caret создаем событие на клик
+    this.parentElement.querySelectorAll(".nested")                        // Ищем все дочерние элементы с классом nested
+                      .forEach((elem)=>elem.classList.toggle("active"));  // Для каждого эелемента назначаем событие, добавляющее/убирающее класс active
+    this.classList.toggle("caret-down");                                  // Добавляем/убираем класс caret-down у раскрывающегося элемента
   });
 }
--- src.js
+++ src.js
@@ -1,37 +1,37 @@
 const http = new XMLHttpRequest();
 
-http.open("GET", "https://raw.githubusercontent.com/wrike/frontend-test/master/data.json",false);
-http.send();
+// http.open("GET", "test1.json",false); // Тестовый JSON файл
+http.open("GET", "https://raw.githubusercontent.com/wrike/frontend-test/master/data.json",false); // Отправляем get запрос на url
+http.send(); // Получаем запрос
 
-const tree = JSON.parse(http.responseText);
+const tree = JSON.parse(http.responseText); //  Парсим JSON и получаем массив объектов
 
-console.log(tree);
+// console.log(tree); // Для отладки
 
-let sortTree = Object.values(tree).sort(function (a,b) {
-  if (a.parentId==null) {
+let sortTree = tree.sort(function (a,b) {    // Сортируем массив по возрастанию свойства parentId, 
+  if (a.parentId==null) {                    // null помещаем в начало массива;
     return -1;
   }
   return a.parentId-b.parentId;
 });
 
-console.log(sortTree);
+// console.log(sortTree); // Для отладки
 
 sortTree.forEach(function (key) { 
-    // console.log(key.parentId);
-    if (key.parentId==null) {
-      let test = document.getElementById('test');
-      let ulObj = document.createElement('ul');
+    if (key.parentId==null) {                         // Создание корневой папки;
+      let test = document.getElementById('tree');     // Создание элементов 
+      let ulObj = document.createElement('ul');       // <ul>, <li>, <span> ;
       let liObj = document.createElement('li');
       let span = document.createElement('span');
-      test.appendChild(ulObj); 
-      ulObj.appendChild(liObj); 
-      liObj.appendChild(span);
-      span.innerText = key.title;
-      liObj.id = 'first-level';
+      test.appendChild(ulObj);                        // Формирование списка 
+      ulObj.appendChild(liObj);                       // в div с классом tree;
+      liObj.appendChild(span);                        // Оборачиваем элемент списка в span для 
+      span.innerText = key.title;                     // создания анимации сворачивания/разворачивания;
+      liObj.id = 'root';                  
       span.className = 'caret';
     }
     if (key.parentId==-1) {
-      let firstLevel= document.getElementById('first-level');
+      let firstLevel= document.getElementById('root');
       let ulObj = document.createElement('ul');
       let liObj = document.createElement('li');
       let span = document.createElement('span');
@@ -39,17 +39,17 @@ sortTree.forEach(function (key) {
       ulObj.appendChild(liObj); 
       liObj.appendChild(span);
       span.innerText = key.title;
-      liObj.id = 'second-level';
-      span.className = 'caret';
+      liObj.id = `second-level id-${key.id}`;         // К классу добавляем id для дальнейшей 
+      span.className = `caret id-${key.id}`;          // идетификации родителя дочерними элементов
       ulObj.className = 'nested';
     }
     if (key.parentId>=1) {
-        let secondLevel = document.getElementById('second-level');
+        let secondLevel = document.getElementById(`second-level id-${key.parentId}`); // Ищем родительский элемент по id
         let liObj = document.createElement('li');
         let ulObj = document.createElement('ul');
         liObj.innerText = key.title;
         secondLevel.appendChild(ulObj); 
         ulObj.appendChild(liObj);
-        ulObj.className = 'nested';
+        ulObj.className = 'nested active'; 
     }
 });
